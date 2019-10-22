9ec15351e1a93663c122d472af8be111a63db5fc
DReyna <alphateam.sistemas@upeu.edu.pe>
2019-10-22 11:22:02 -0500

    proyecto base

---
+++ src/test/java/com/musa1/Musa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Musa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4cc1e54dae98d56125250a2484e15dd24e49fc14
gabrielcosi <gabrielcosi18@gmail.com>
2019-10-22 16:20:23 -0500

    entidad tipo_documento

---
+++ src/main/java/com/musa1/entity/Tipo_documento.java
@@ -0,0 +1,25 @@
+package com.musa1.entity;
+
+public class Tipo_documento {
+	int id_tipo_documento;
+	String nombre_documento;
+	public Tipo_documento(int id_tipo_documento, String nombre_documento) {
+		super();
+		this.id_tipo_documento = id_tipo_documento;
+		this.nombre_documento = nombre_documento;
+	}
+	public int getId_tipo_documento() {
+		return id_tipo_documento;
+	}
+	public void setId_tipo_documento(int id_tipo_documento) {
+		this.id_tipo_documento = id_tipo_documento;
+	}
+	public String getNombre_documento() {
+		return nombre_documento;
+	}
+	public void setNombre_documento(String nombre_documento) {
+		this.nombre_documento = nombre_documento;
+	}
+	
+	
+}

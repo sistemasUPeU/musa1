2b173d1005369dc4ee46801c0e2aab4c03a61bb4
DReyna <alphateam.sistemas@upeu.edu.pe>
2019-10-22 12:15:23 -0500

    prueba

--- src/main/java/com/musa1/controller/HomeController.java
+++ src/main/java/com/musa1/controller/HomeController.java
@@ -1,7 +1,7 @@
 package com.musa1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +14,7 @@ import com.musa1.service.PruebaService;
 public class HomeController {
   @Autowired
   PruebaService pruebaService;
+  @GetMapping
   public String get() {
 	  return pruebaService.listar("Proyecto Musa");
   }

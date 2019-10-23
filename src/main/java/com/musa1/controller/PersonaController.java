bbb4efb96524d8e336d2400982aa96e1a3117a7e
gabrielcosi <gabrielcosi18@gmail.com>
2019-10-22 23:31:55 -0500

    Persona

--- src/main/java/com/musa1/controller/PersonaController.java
+++ src/main/java/com/musa1/controller/PersonaController.java
@@ -3,8 +3,11 @@ package com.musa1.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +28,18 @@ public class PersonaController {
 	@PostMapping("/add")
 	public int save(@RequestBody Persona persona) {
 		return personaService.create(persona);
+	}
+	@DeleteMapping("/{id}")
+	public int delete(@PathVariable int id) {
+		return personaService.delete(id);
+	}
+	@GetMapping("/{id}")
+	public Map<String, Object> read(@PathVariable int id) {
+		return personaService.read(id);
+	}
+	@PutMapping("/{id}")
+	public int update(@RequestBody Persona per, @PathVariable int id) {
+		per.setId_persona(id);
+		return personaService.edit(per);
 	}
 }
--- src/main/java/com/musa1/daoImp/PersonaDaoImp.java
+++ src/main/java/com/musa1/daoImp/PersonaDaoImp.java
@@ -1,11 +1,15 @@
 package com.musa1.daoImp;
 
+import java.sql.Types;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.ColumnMapRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.SqlOutParameter;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
 
@@ -28,23 +32,28 @@ public class PersonaDaoImp implements PersonaDao {
 	@Override
 	public int edit(Persona persona) {
 		// TODO Auto-generated method stub
-		return 0;
+		return jdbcTemplate.update("call pkg_crud_persona.sp_update_persona(?,?,?,?,?,?,?)",persona.getId_persona(),persona.getNombre_persona(),persona.getApellido_paterno(),persona.getApellido_materno(),persona.getNro_documento(),persona.getCelular(),persona.getId_tipo_documento());
 	}
 	@Override
 	public int delete(int id) {
 		// TODO Auto-generated method stub
-		return 0;
+		return jdbcTemplate.update("call pkg_crud_persona.sp_delete_persona(?)",id);
 	}
 	@Override
 	public Map<String, Object> read(int id) {
 		// TODO Auto-generated method stub
-		return null;
+		simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
+				.withProcedureName("SP_READ_PERSONA").withCatalogName("pkg_crud_persona")
+				.declareParameters(new SqlOutParameter("pers",OracleTypes
+						.CURSOR,new ColumnMapRowMapper()), new SqlParameter("idp", Types.INTEGER));
+				SqlParameterSource in = new MapSqlParameterSource().addValue("idp", id);
+		return simpleJdbcCall.execute(in);
 	}
 	@Override
 	public Map<String, Object> readAll() {
 		// TODO Auto-generated method stub
-		simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate).withProcedureName("SP_LIST_PERSONA")
-				.declareParameters(new SqlOutParameter("per", OracleTypes.CURSOR, new ColumnMapRowMapper()));
+		simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate).withProcedureName("SP_READALL_PERSONA").withCatalogName("pkg_crud_persona")
+				.declareParameters(new SqlOutParameter("pers", OracleTypes.CURSOR, new ColumnMapRowMapper()));
 		return simpleJdbcCall.execute();
 	}
 	

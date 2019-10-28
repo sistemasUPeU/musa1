f4c53c8fedff8f52193d3ebf535d87abf7348c1c
johan <johanmamani@upeu.edu.pe>
2019-10-27 23:31:59 -0500

    Agregue BusDao

---
+++ src/main/java/com/musa1/dao/BusDao.java
@@ -0,0 +1,14 @@
+package com.musa1.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.musa1.entity.Bus;
+
+public interface BusDao {
+	public int create(Bus b);
+	public int update(Bus b);
+	public int delete(int id);
+	public Bus read(int id);
+	List<Map<String, Object>> readAll();
+}

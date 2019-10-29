d49f3f31fb9d8446d8a2339c70bd0f028150cca5
gabrielcosi <gabrielcosi18@gmail.com>
2019-10-29 16:00:44 -0500

    Requisito

--- src/main/java/com/musa1/dao/RequisitoDao.java
+++ src/main/java/com/musa1/dao/RequisitoDao.java
@@ -2,6 +2,13 @@ package com.musa1.dao;
 
 import java.util.Map;
 
+import com.musa1.entity.Requisito;
+
 public interface RequisitoDao {
+	public int create (Requisito requisito);
+	public int update (Requisito requisito);
+	public Map<String, Object> read(int id);
+	public Map<String, Object> readAll();
+	public Map<String, Object> readRequisitoConductor();
 	public Map<String, Object> listarRequisitosBus();
 }

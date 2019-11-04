9244d73b45347cff0439757f84b9066b6ef3d1b9
JoseCV09 <josechacpa@upeu.edu.pe>
2019-11-04 12:08:34 -0500

    Mantenimiento 100

---
+++ src/main/java/com/musa1/controller/MantenimientoController.java
@@ -0,0 +1,43 @@
+package com.musa1.controller;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.musa1.entity.Mantenimiento;
+import com.musa1.service.MantenimientoService;
+
+@RestController
+@RequestMapping("/mantenimiento")
+public class MantenimientoController {
+	@Autowired
+	private MantenimientoService mantenimientoService;
+	
+	@GetMapping("/")
+	public Map<String, Object> getall(){
+		return mantenimientoService.readAll();
+	}
+	@GetMapping("/{id}")
+	public Map<String, Object> getread(@PathVariable int id){
+		return mantenimientoService.read(id);
+	}
+	@PostMapping("/add")
+	public int save(@RequestBody Mantenimiento mantenimiento) {
+		return mantenimientoService.create(mantenimiento);
+	}
+	@PutMapping("/{id}")
+	public int update(@RequestBody Mantenimiento mantenimiento, @PathVariable int id) {
+		mantenimiento.setId_tipo_mantenimiento(id);
+		return mantenimientoService.update(mantenimiento);
+	}
+	@DeleteMapping("/{id}")
+	public int delete(@PathVariable int id) {
+		return mantenimientoService.delete(id);
+	}
+}

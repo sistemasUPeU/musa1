7c870e55eb2a04a799ebfc94b150d612133ba9d6
johan <johanmamani@upeu.edu.pe>
2019-11-05 17:19:22 -0500

    crossorigin

--- src/main/java/com/musa1/controller/BusController.java
+++ src/main/java/com/musa1/controller/BusController.java
@@ -3,6 +3,7 @@ package com.musa1.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.musa1.entity.Bus;
 import com.musa1.service.BusService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/bus")
 public class BusController {

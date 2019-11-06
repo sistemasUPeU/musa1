e653ee8962b884725d379d55865807f9d0505d5f
Acuario11 <madaichuco@upeu.edu.pe>
2019-11-06 07:51:37 -0500

    Producto 100%

--- src/main/java/com/musa1/daoImp/ProductoDaoImp.java
+++ src/main/java/com/musa1/daoImp/ProductoDaoImp.java
@@ -11,12 +11,13 @@ import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.stereotype.Repository;
 
 import com.musa1.dao.ProductoDao;
 import com.musa1.entity.Producto;
 
 import oracle.jdbc.internal.OracleTypes;
-
+@Repository
 public class ProductoDaoImp implements ProductoDao{
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
@@ -25,36 +26,36 @@ public class ProductoDaoImp implements ProductoDao{
 	@Override
 	public int create(Producto producto) {
 		// TODO Auto-generated method stub
-		return jdbcTemplate.update("call pkg_crud_producto.sp_insertar_producto(?,?,?,?,?,?)",producto.getNombre_producto(),producto.getDescripcion(),producto.getId_categoria(),producto.getId_unidad_medida(),producto.getCodigo(),producto.getId_marca());
+		return jdbcTemplate.update("call PKG_CRUD_PRODUCTO.SP_INSERTAR_PRODUCTO(?,?,?,?,?,?)",producto.getNombre_producto(),producto.getDescripcion(),producto.getId_categoria(),producto.getId_unidad_medida(),producto.getCodigo(),producto.getId_marca());
 	}
 
 	@Override
 	public int edit(Producto producto) {
 		// TODO Auto-generated method stub	
-		return jdbcTemplate.update("call pkg_crud_producto.sp_actualizar_producto(?,?,?,?,?,?,?)",producto.getId_producto(),producto.getNombre_producto(),producto.getDescripcion(),producto.getId_categoria(),producto.getId_unidad_medida(),producto.getCodigo(),producto.getId_marca());
+		return jdbcTemplate.update("call PKG_CRUD_PRODUCTO.SP_ACTUALIZAR_PRODUCTO(?,?,?,?,?,?,?)",producto.getId_producto(),producto.getNombre_producto(),producto.getDescripcion(),producto.getId_categoria(),producto.getId_unidad_medida(),producto.getCodigo(),producto.getId_marca());
 	}
 
 	@Override
 	public int delete(int id) {
 		// TODO Auto-generated method stub
-		return jdbcTemplate.update("call pkg_crud_producto.sp_eliminar_producto(?)",id);
+		return jdbcTemplate.update("call PKG_CRUD_PRODUCTO.SP_ELIMINAR_PRODUCTO(?)",id);
 	}
 
 	@Override
 	public Map<String, Object> read(int id) {
 		// TODO Auto-generated method stub
 		simpleJdbcCall =new SimpleJdbcCall(jdbcTemplate)
-				.withProcedureName("sp_read_producto").withCatalogName("pkg_crud_producto")
-				.declareParameters(new SqlOutParameter("lis_prod",OracleTypes.CURSOR,new ColumnMapRowMapper()),new SqlParameter("id_prod", Types.INTEGER));
-				SqlParameterSource in =new MapSqlParameterSource().addValue("id_prod", id);
+				.withProcedureName("SP_READ_PRODUCTO").withCatalogName("PKG_CRUD_PRODUCTO")
+				.declareParameters(new SqlOutParameter("LIS_PROD	",OracleTypes.CURSOR,new ColumnMapRowMapper()),new SqlParameter("PROD_ID", Types.INTEGER));
+				SqlParameterSource in =new MapSqlParameterSource().addValue("PROD_ID", id);
 		return simpleJdbcCall.execute(in);
 	}
 
 	@Override
 	public Map<String, Object> readAll() {
 		// TODO Auto-generated method stub
-		simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate).withProcedureName("sp_consulta_producto").withCatalogName("pkg_crud_producto")
-				.declareParameters(new SqlOutParameter("lis_prod", OracleTypes.CURSOR, new ColumnMapRowMapper()));
+		simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate).withProcedureName("SP_CONSULTA_PRODUCTO").withCatalogName("PKG_CRUD_PRODUCTO")
+				.declareParameters(new SqlOutParameter("LIS_PROD", OracleTypes.CURSOR, new ColumnMapRowMapper()));
 		return simpleJdbcCall.execute();
 	}
 

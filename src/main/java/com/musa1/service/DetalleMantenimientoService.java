7fee6584f72672c2c362e52a322bd84ed791169e
JoseCV09 <josechacpa@upeu.edu.pe>
2019-11-06 07:23:15 -0500

    DetalleMantenimiento 80%

--- src/main/java/com/musa1/service/DetalleMantenimientoService.java
+++ src/main/java/com/musa1/service/DetalleMantenimientoService.java
@@ -1,11 +1,13 @@
-package com.musa1.dao;
+package com.musa1.service;
+
 import java.util.Map;
+
 import com.musa1.entity.DetalleMantenimiento;
-public interface DetalleMantenimientoDao {
+
+public interface DetalleMantenimientoService {
 	public int create(DetalleMantenimiento detallemantenimiento);
 	public int update(DetalleMantenimiento detallemantenimiento);
 	public int delete(int id);
 	public Map<String, Object> read(int id);
 	public Map<String, Object> readAll();
 }
-	

29021060e4f2520bd52b499d4ce3a8b527cd97e2
Dany <danyharo@upeu.edu.pe>
2019-11-08 19:51:53 +0100

    arregle algunas cosas

--- src/main/java/com/musa1/entity/TipoAccion.java
+++ src/main/java/com/musa1/entity/TipoAccion.java
@@ -2,16 +2,16 @@ package com.musa1.entity;
 
 public class TipoAccion {
 	private int id_tipo_accion;
-	private String tipo_accion;
+	private String nom_tipo_accion;
 	private String estado;
 	public TipoAccion() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public TipoAccion(int id_tipo_accion, String tipo_accion, String estado) {
+	public TipoAccion(int id_tipo_accion, String nom_tipo_accion, String estado) {
 		super();
 		this.id_tipo_accion = id_tipo_accion;
-		this.tipo_accion = tipo_accion;
+		this.nom_tipo_accion = nom_tipo_accion;
 		this.estado = estado;
 	}
 	public int getId_tipo_accion() {
@@ -20,11 +20,11 @@ public class TipoAccion {
 	public void setId_tipo_accion(int id_tipo_accion) {
 		this.id_tipo_accion = id_tipo_accion;
 	}
-	public String getTipo_accion() {
-		return tipo_accion;
+	public String getNom_tipo_accion() {
+		return nom_tipo_accion;
 	}
-	public void setTipo_accion(String tipo_accion) {
-		this.tipo_accion = tipo_accion;
+	public void setNom_tipo_accion(String nom_tipo_accion) {
+		this.nom_tipo_accion = nom_tipo_accion;
 	}
 	public String getEstado() {
 		return estado;
@@ -32,5 +32,6 @@ public class TipoAccion {
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
+	
 
 }

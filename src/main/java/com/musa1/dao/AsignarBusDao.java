75badfc3ec3c88dad93e391bd095b971fbfe3703
Adrian <adrianorihuela@upeu.edu.pe>
2019-11-14 08:41:53 -0500

    Soy el chef y la he estado c..

--- src/main/java/com/musa1/dao/AsignarBusDao.java
+++ src/main/java/com/musa1/dao/AsignarBusDao.java
@@ -11,13 +11,8 @@ public interface AsignarBusDao {
 	public Map<String, Object> readParadero();
 
 	public int edit(AsignarBus ab);
-<<<<<<< HEAD
-	
 
 	Map<String , Object> readfil(int nume);
 
-=======
->>>>>>> a5087daa848d2f98712c78bce8878e98dc420fae
 
-	Map<String , Object> readfil(int nume);
 }
--- src/main/java/com/musa1/service/AsignarBusService.java
+++ src/main/java/com/musa1/service/AsignarBusService.java
@@ -10,11 +10,8 @@ public interface AsignarBusService {
 	public Map<String, Object> readAsignarBus();
 	public Map<String, Object> readParadero();
 	public int edit(AsignarBus ab);
-<<<<<<< HEAD
 
 	Map<String , Object> readfil(int nume);
 
-=======
-	Map<String , Object> readfil(int nume);
->>>>>>> a5087daa848d2f98712c78bce8878e98dc420fae
+
 }
